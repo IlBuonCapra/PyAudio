2ea6e291f31c1454fa191c0f5d3421d0ec5333b4
Playhelpo <legolego@live.it>
2017-07-27 14:24:18 +0200

    Update main.py version 0.2

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 # -*- coding: utf-8 -*-
-
 import speech_recognition as sr
-from time import ctime,sleep
+import time as t
 import pyttsx as se
 
+HOUR = t.strftime("%X")
+DATE = str(t.strftime("%d %B %Y"))
+TURN_OFF = "schiavo = False"
 VOLUME = 1
 RATE = 200
 NAME = "JARVIS"
-DRIVER = 'espeak'
+DRIVER = 'sapi5'#'espeak'
 LANGUAGE = 'italian'
 
-l = sr.Recognizer()
+possible_commands = {
+    "spegniti" : "{NAME} si sta spegnendo!",
+    "ciao" : "Ciao!",
+    "come va" : "Bene grazie!",
+    "data" : "Oggi è {DATE}",
+    "ora" : "Sono le {HOUR}",
+    "chiami" : "Mi chiamo {NAME}",
+    "grazie" : "Figurati!",
+    "porcodio" : "Diocane!"
+}
 
-schiavo = True
+def make_action(command, possible_commands=possible_commands):
+    if command in possible_commands.keys():
+        speak(possible_commands[command].format(command))
 
 def speak(text):
     print(NAME + " >> " + text)
@@ -34,31 +47,17 @@ def listen():
         print("You >> " + sound)
         return sound
     except sr.UnknownValueError:
-        print("Non ho capito")
-        #print("Could not understand audio")
-    except sr.RequestError as e:
-        print("Errore {0}".format(e))
-
-    return ""
+        return ""
 
 if __name__ == '__main__':
+    l = sr.Recognizer()
+    schiavo = True
+
+    #test()
+    #schiavo = False
+
+    if schiavo:
+        speak(NAME + " si stà accendendo")
     while schiavo:
         listening = listen()
-        if "spegni" in listening:
-            speak("CI STANNO TRACCIANDO! STACCA STACCA!")
-            schiavo = False
-
-        if "ciao" in listening:
-            speak("Ciao")
-
-        if "come va" in listening:
-            speak("Bene grazie!")
-
-        if "dimmi l'ora" in listening:
-            speak(ctime())
-
-        if "come ti chiami" in listening:
-            speak("mi chiamo " + NAME)
-
-        if "grazie" in listening:
-            speak("figurati. dovere")
+        make_action(listening)
